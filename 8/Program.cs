90e30bc0fc1053ec79ee1770dfe16ec4d50ac47c
KrzysztofBojarczuk <69596796+KrzysztofBojarczuk@users.noreply.github.com>
2021-04-13 09:41:43 +0200

    Add project files.

---
+++ 8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            Console.WriteLine("Wprowadź długośc przyprostokątnej A.");
+            a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Wprowadź długość przyprostokątnej B.");
+            b = double.Parse(Console.ReadLine());
+
+            c = Math.Sqrt(Math.Pow(a,2) + Math.Pow(b, 2));
+
+            Console.WriteLine($"Długość przeciwprostokątnej {c}.");
+        }
+    }
+}
